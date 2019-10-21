83cae3a25245aa9c356581ba27c74d9be936d0b2
alihan21 <alihan.fevziev@student.hogent.be>
2019-10-21 12:48:29 +0200

    Add Domain (classes and properties)

---
+++ FlightApp.Backend/Models/Domain/FlightStaff.cs
@@ -0,0 +1,13 @@
+﻿namespace FlightApp.Backend.Models.Domain
+{
+    public class FlightStaff : User
+    {
+        public int LoginId { get; set; }
+        public Flight Flight { get; set; }
+
+        public void MovePassengerToAnotherSeat(Passenger passenger, Seat seat)
+        {
+            passenger.Seat = seat;
+        }
+    }
+}
---
+++ FlightApp.Backend/Models/Domain/User.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace FlightApp.Backend.Models.Domain
+{
+    public class User : IdentityUser
+    {
+
+    }
+}
