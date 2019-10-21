ffcf2e44eea7002f4cc89f0f15af0bf997a91d80
alihan21 <alihan.fevziev@student.hogent.be>
2019-10-21 11:25:49 +0200

    Make dev branch

--- FlightApp.Backend/Models/ErrorViewModel.cs
+++ FlightApp.Backend/Models/ErrorViewModel.cs
@@ -6,6 +6,6 @@ namespace FlightApp.Backend.Models
     {
         public string RequestId { get; set; }
 
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId );
     }
 }
