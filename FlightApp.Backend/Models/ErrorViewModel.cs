ed4dda040bd938a675e8f7a97cb168eb3a299f58
alihan21 <alihan.fevziev@student.hogent.be>
2019-10-21 11:23:44 +0200

    InitBackend

---
+++ FlightApp.Backend/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace FlightApp.Backend.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
