e05a4e0fdd841e3e12c15603ca8ae145a434dbfd
alihan21 <alihan.fevziev@student.hogent.be>
2019-11-12 00:23:32 +0100

    Make UWP project & Design LoginPassenger and LoginStaff

---
+++ FlightApp.Frontend/Pages/LoginPassenger.xaml.cs
@@ -0,0 +1,21 @@
+﻿using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace FlightApp.Frontend.Pages
+{
+    /// <summary>
+    /// This is the page login page for passenger
+    /// </summary>
+    public sealed partial class LoginPassenger : Page
+    {
+        public LoginPassenger()
+        {
+            InitializeComponent();
+        }
+
+        private void navigateToStaffmemberPage(object sender, RoutedEventArgs e)
+        {
+            Frame.Navigate(typeof(LoginStaff));
+        }
+    }
+}
---
+++ FlightApp.Frontend/Pages/LoginStaff.xaml.cs
@@ -0,0 +1,23 @@
+﻿using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace FlightApp.Frontend.Pages
+{
+    /// <summary>
+    /// This is where a staffmember can login
+    /// </summary>
+    public sealed partial class LoginStaff : Page
+    {
+        public LoginStaff()
+        {
+            InitializeComponent();
+        }
+
+        private void NavigateToPassengerPage(object sender, RoutedEventArgs e)
+        {
+            Frame.Navigate(typeof(LoginPassenger));
+        }
+    }
+}
