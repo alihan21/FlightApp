b106612956f517c5f8e30605447d4104d552e696
Dean Van Damme <dean.vandamme.y9427@student.hogent.be>
2019-11-13 16:43:57 +0100

    implement SeatController

--- FlightApp.Backend/Controllers/SeatController.cs
+++ FlightApp.Backend/Controllers/SeatController.cs
@@ -2,15 +2,37 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using FlightApp.Backend.Models.Domain;
 using Microsoft.AspNetCore.Mvc;
 
 namespace FlightApp.Backend.Controllers
 {
+
+    [Route("api/[controller]")]
+    [ApiController]
     public class SeatController : Controller
     {
-        public IActionResult Index()
+        private readonly ISeatRepository _seatRepository;
+
+
+        public SeatController(ISeatRepository context)
         {
-            return View();
+            _seatRepository = context;
+        }
+
+        [HttpGet]
+        public IEnumerable<Seat> GetUsers()
+        {
+            return _seatRepository.GetAll();
+        }
+
+
+        [HttpGet("{id}")]
+        public ActionResult<Seat> GetSeat(int id)
+        {
+            Seat seat = _seatRepository.GetBy(id);
+            if (seat == null) return NotFound();
+            return seat;
         }
     }
 }
