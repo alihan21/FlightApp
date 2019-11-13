b7f8fb11aa73bfc6e6a7329fa1c794d1f835f4e9
Dean Van Damme <dean.vandamme.y9427@student.hogent.be>
2019-11-13 09:07:33 +0100

    Implement IUserRepository

--- FlightApp.Backend/Models/Domain/IUserRepository.cs
+++ FlightApp.Backend/Models/Domain/IUserRepository.cs
@@ -7,6 +7,16 @@ namespace FlightApp.Backend.Models.Domain
 {
     public interface IUserRepository
     {
-        
+        User GetBy(int id);
+
+        IEnumerable<User> GetAll();
+
+        void Add(User user);
+
+        void Delete(User user);
+
+        void Update(User user);
+
+        void SaveChanges();
     }
 }
