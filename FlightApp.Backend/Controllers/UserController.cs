f1d3ff7dcbc75d123512fd40e01d74c35ad4d244
Dean Van Damme <dean.vandamme.y9427@student.hogent.be>
2019-11-13 16:23:47 +0100

    Add swagger and implement Usercontroller

--- FlightApp.Backend/Controllers/UserController.cs
+++ FlightApp.Backend/Controllers/UserController.cs
@@ -2,15 +2,58 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using FlightApp.Backend.Models.Domain;
 using Microsoft.AspNetCore.Mvc;
 
 namespace FlightApp.Backend.Controllers
 {
+    [Route("api/[controller]")]
+    [ApiController]
     public class UserController : Controller
     {
-        public IActionResult Index()
+
+        private readonly IUserRepository _userRepository;
+
+
+        public UserController(IUserRepository context)
         {
-            return View();
+                _userRepository = context;
         }
+
+
+        // GET: api/Users
+        /// <summary>
+        /// Get all recipes ordered by name
+        /// </summary>
+        [HttpGet]
+        public IEnumerable<User> GetUsers()
+        {
+                return _userRepository.GetAll();
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<User> GetUser(int id)
+        {
+            User user = _userRepository.GetBy(id);
+            if (user == null) return NotFound();
+            return user;
+        }
+
+        /// <summary>
+        /// Modifies a user
+        /// </summary>
+        /// 
+        [HttpPut("{id}")]
+        public IActionResult PutRecipe(int id, User user)
+        {
+            if (id != user.UserId)
+            {
+                return BadRequest();
+            }
+            _userRepository.Update(user);
+            _userRepository.SaveChanges();
+            return NoContent();
+        }
+
     }
 }
--- FlightApp.Backend/Startup.cs
+++ FlightApp.Backend/Startup.cs
@@ -21,12 +21,6 @@ namespace FlightApp.Backend
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.Configure<CookiePolicyOptions>(options =>
-            {
-                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
-                options.CheckConsentNeeded = context => true;
-                options.MinimumSameSitePolicy = SameSiteMode.None;
-            });
 
             services.AddDbContext<FlightAppContext>(options => {
                 options.UseSqlServer(Configuration.GetConnectionString("DevConnection"));
@@ -35,6 +29,17 @@ namespace FlightApp.Backend
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
 
             services.AddScoped<FlightAppInit>();
+
+
+
+            services.AddOpenApiDocument(c =>
+            {
+                c.DocumentName = "apidocs";
+                c.Title = "FlightApp API";
+                c.Version = "v1";
+                c.Description = "The FlightApp API documentation description.";
+            });
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -55,12 +60,12 @@ namespace FlightApp.Backend
             app.UseStaticFiles();
             app.UseCookiePolicy();
 
-            app.UseMvc(routes =>
-            {
-                routes.MapRoute(
-                    name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
-            });
+
+
+            app.UseMvc();
+
+            app.UseSwaggerUi3();
+            app.UseSwagger();
 
             flightAppInit.Init();
         }
