58ddd0a987090ca284684b51c3c5cc50fa75fd59
Dean Van Damme <dean.vandamme.y9427@student.hogent.be>
2019-11-13 09:10:24 +0100

    Implement IPlaneRepository

--- FlightApp.Backend/Models/Domain/IPlaneRepository.cs
+++ FlightApp.Backend/Models/Domain/IPlaneRepository.cs
@@ -7,5 +7,15 @@ namespace FlightApp.Backend.Models.Domain
 {
     public interface IPlaneRepository
     {
+
+        Plane GetBy(int id);
+
+        IEnumerable<Plane> GetAll();
+
+        void Add(Plane plane);
+
+        void Update(Plane plane);
+
+        void SaveChanges();
     }
 }
