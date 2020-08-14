57a2a95f49640a517f8714852786d3caeda0ca4d
Dean Van Damme <dean.vandamme.y9427@student.hogent.be>
2020-08-14 20:47:47 +0200

    Add message model to frontend

---
+++ FlightApp.Frontend/Models/Message.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlightApp.Frontend.Models
+{
+    public class Message
+    {
+        public int MessageId { get; set; }
+        public string Text { get; set; }
+        public int ChannelId { get; set; }
+        public Passenger Passenger { get; set; }
+
+        public Message()
+        {
+
+        }
+
+        public Message(Passenger passenger, string text)
+        {
+            Text = text;
+            Passenger = passenger;
+            ChannelId = passenger.ChannelId;
+        }
+    }
+
+}
