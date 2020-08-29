2f2c06a4f72b1b670ca6b1c01f3b272401fd3459
alihan21 <alihan.fevziev@student.hogent.be>
2020-08-29 20:55:42 +0200

    Add styling & change language to english & small fixes

--- FlightApp.Backend/Models/DTO's/EntertainmentDTO.cs
+++ FlightApp.Backend/Models/DTO's/EntertainmentDTO.cs
@@ -6,8 +6,6 @@ namespace FlightApp.Backend.Models.DTO_s
     public class EntertainmentDTO
     {
         public string Title { get; set; }
-        public string Year { get; set; }
-        public string PG { get; set; }
         public string Released { get; set; }
         public string Runtime { get; set; }
         public string Genre { get; set; }
@@ -19,24 +17,11 @@ namespace FlightApp.Backend.Models.DTO_s
         public string Country { get; set; }
         public string Awards { get; set; }
         public string Poster { get; set; }
-        public List<RatingDTO> Ratings { get; set; }
-        public string Metascore { get; set; }
-        public string ImdbRating { get; set; }
-        public string ImdbVotes { get; set; }
-        public string ImdbId { get; set; }
         public string Type { get; set; }
-        public string DVD { get; set; }
-        public string BoxOffice { get; set; }
-        public string Production { get; set; }
-        public string Website { get; set; }
-        public bool Response { get; set; }
 
         public EntertainmentDTO(Entertainment entertainment)
         {
-            Ratings = new List<RatingDTO>();
             Title = entertainment.Title;
-            Year = entertainment.Year;
-            PG = entertainment.PG;
             Released = entertainment.Released;
             Runtime = entertainment.Runtime;
             Genre = entertainment.Genre;
@@ -48,35 +33,7 @@ namespace FlightApp.Backend.Models.DTO_s
             Country = entertainment.Country;
             Awards = entertainment.Awards;
             Poster = entertainment.Poster;
-            if(entertainment.Ratings.Count > 0)
-            {
-                foreach (Rating rating in entertainment.Ratings)
-                {
-                    Ratings.Add(new RatingDTO(rating));
-                }
-            }
-            Metascore = entertainment.Metascore;
-            ImdbRating = entertainment.ImdbRating;
-            ImdbVotes = entertainment.ImdbVotes;
-            ImdbId = entertainment.ImdbId;
             Type = entertainment.Type;
-            DVD = entertainment.DVD;
-            BoxOffice = entertainment.BoxOffice;
-            Production = entertainment.Production;
-            Website = entertainment.Website;
-            Response = entertainment.Response;
-        }
-
-        public class RatingDTO
-        {
-            public string Source { get; set; }
-            public string Value { get; set; }
-
-            public RatingDTO(Rating rating)
-            {
-                Source = rating.Source;
-                Value = rating.Value;
-            }
         }
     }
 }
